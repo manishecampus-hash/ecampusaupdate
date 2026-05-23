abcbd258d65f4605f133fe7632bf8e54b02d4e4a
Manish <manish.ecampus@gmail.com>
2026-05-23 14:52:08 +0530

    banner added

--- components/ui/header-3.tsx
+++ components/ui/header-3.tsx
@@ -1025,7 +1025,11 @@ function NavContent({
         onClick={closeAll}
         className="flex items-center gap-2 hover:opacity-80 select-none flex-shrink-0"
       >
-        <img src="/logo.png" alt="eCampus" className="h-10 w-auto" />
+        <img
+          src="/logo.png"
+          alt="eCampus"
+          className="h-10 w-auto brightness-0"
+        />
       </Link>
 
       <div className="hidden md:flex items-center gap-1">
